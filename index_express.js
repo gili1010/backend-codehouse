97d4c4d7b5bed3abbebd7f5827acb888cc02d7e2
gili1010 <lucianog_332@hotmail.com>
2021-11-21 22:33:13 -0300

    ss2

--- index_express.js
+++ index_express.js
@@ -4,21 +4,19 @@ const { response } = require("express");
 const express = require("express")
 
 const app = express()
-const PORT1 = 8080
+const PORT = 8080
 
 let file = "produto.json"
 let lista = []
 
 /* .listen(process.env.PORT || 8080) */
-app.listen(process.env.PORT || PORT1 , () => {
-    console.log(`servidor corriendo en ${PORT1}`);
+app.listen(process.env.PORT || PORT , () => {
+    console.log(`servidor corriendo en ${PORT}`);
 })
 
  app.get ('/', (request, response) =>{
     response.send("Hola a todos")
-    let data = fs.promises.readFile(file)
-            lista = JSON.parse(data)
-            console.log(lista);
+
 })
 
 app.get ('/productoramdon', (request, response) =>{
