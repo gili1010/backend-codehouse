b145a559102953b9418b2b2b0001a2f715c587d7
gili1010 <lucianog_332@hotmail.com>
2021-11-21 22:28:13 -0300

    port1

---
+++ contenedor.js
@@ -0,0 +1,85 @@
+const fs = require('fs')
+
+class Contenedor {
+    constructor(){
+        console.log("iniciando contenedor");
+
+    }
+
+    static countId = 0
+    static file = "produto.json"
+    static lista = [{hola:"hokla"}]
+
+/*     //metodo save: recibe un objeto, lo guarda y devuelve el id asignado
+    async save (objeto) {
+        Contenedor.countId++;
+        objeto["id"] = Contenedor.countId
+
+        Contenedor.lista.push(objeto)
+
+        await this.write()
+
+        return Contenedor.countId;
+    } */
+
+
+/*     //metodo deleteById : elimina del archivo el objeto con el id buscado.
+    async deleteById(id){
+
+        Contenedor.lista = Contenedor.lista.filter(element => element.id != id)
+
+        await this.write()
+    } */
+
+/*     //metodo deleteAll : elimina todos los objetos presentes.
+    async deleteAll(){
+
+        fs.unlink("./contenedor.json", error =>{
+            if(error) {
+                console.log("hubo un error");
+            }else {
+                console.log("borrado");
+            }
+        })
+
+            } */
+
+    async write(){
+
+        let string = JSON.stringify(Contenedor.lista)
+        await fs.promises.writeFile(Contenedor.file, string)
+    }
+
+    
+    //metodo getAll devuelve un array conlos objetos presentes en el archivo
+    getAll(){
+        return Contenedor.lista;
+        }
+
+/* 
+        //metodo getById : recibe un id y devuelve el objeto con ese id
+        async getById(id){
+
+            Contenedor.lista = Contenedor.lista.filter(element => element.id == id)
+    
+            console.log(Contenedor.lista);
+        } */
+
+    async init(){
+        try {
+            let data = await fs.promises.readFile(Contenedor.file)
+            Contenedor.lista = JSON.parse(data)
+    
+/*             for (const element of Contenedor.lista){
+                if(element.id > Contenedor.countId) 
+                Contenedor.countId = element.id
+            }
+            console.log("Last ID: ", Contenedor.countId)  */
+        } catch (error){
+            console.log("Aun no hay archivo");
+        }
+
+    }
+}
+
+module.exports = Contenedor
--- index_express.js
+++ index_express.js
@@ -1,17 +1,26 @@
+const Contenedor = require ('./contenedor.js')
+
 const { response } = require("express");
 const express = require("express")
 
 const app = express()
-const PORT = 3000
+const PORT1 = 8080
 
-const server = app.listen(PORT, () => {
-    console.log(`servidor corriendo en ${PORT}`);
+let file = "produto.json"
+let lista = []
+
+/* .listen(process.env.PORT || 8080) */
+app.listen(process.env.PORT || PORT1 , () => {
+    console.log(`servidor corriendo en ${PORT1}`);
 })
 
-server.on('error', error =>{
-    console.log("error de servidor");
-})
-
-app.get ('/', (request, response) =>{
+ app.get ('/', (request, response) =>{
     response.send("Hola a todos")
+    let data = fs.promises.readFile(file)
+            lista = JSON.parse(data)
+            console.log(lista);
+})
+
+app.get ('/productoramdon', (request, response) =>{
+    response.send("Hola a todos soy ramdon")
 })
